de5a1b3a9d53f7728aed322b66d3f9e50fb53b00
star003 <x00502@gmail.com>
2015-04-24 22:39:01 +0300

    2015-04-24
    из небытия мой первый виджет 2012 года

---
+++ src/weather/com/com/UpdateWidgetService.java
@@ -0,0 +1,65 @@
+package weather.com.com;
+
+import weather.com.com.R;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.RemoteViews;
+
+
+
+public class UpdateWidgetService extends Service {
+	
+	readS y = new readS("http://star003.dlinkddns.com/03.php");
+	
+	@Override
+	public  void onStart(Intent intent, int startId) {
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
+				.getApplicationContext());
+		int[] allWidgetIds = intent
+				.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+		
+		y.getDt();
+		
+	for (int widgetId : allWidgetIds) {
+			RemoteViews remoteViews = new RemoteViews(this
+					.getApplicationContext().getPackageName(),
+					R.layout.widget_layout);
+			
+			String bashtemp = y.cur;
+			
+	 remoteViews.setTextViewText(R.id.update,
+		  					bashtemp); //+"°"
+	 remoteViews.setTextViewText(R.id.td,
+				y.td); //+"°"
+	 remoteViews.setTextViewText(R.id.mn,
+				y.mn); //+"°"
+	 remoteViews.setTextViewText(R.id.mx,
+				y.mx); //+"°"
+	        
+			Intent clickIntent = new Intent(this.getApplicationContext(),
+					MyWidgetProvider.class);
+			clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+			clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+					allWidgetIds);
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(
+					getApplicationContext(), 0, clickIntent,
+					PendingIntent.FLAG_UPDATE_CURRENT);
+			remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+			remoteViews.setOnClickPendingIntent(R.id.td, pendingIntent);
+			remoteViews.setOnClickPendingIntent(R.id.mn, pendingIntent);
+			remoteViews.setOnClickPendingIntent(R.id.mx, pendingIntent);
+			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+		}
+		stopSelf();
+
+		super.onStart(intent, startId);
+	}
+	    
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+}
---
+++ src/weather/com/com/WidgetActivity.java
@@ -0,0 +1,14 @@
+package weather.com.com;
+
+import weather.com.com.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WidgetActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
